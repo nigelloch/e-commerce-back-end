664620a6efa3fce7186fe8b7095c15ee14d1c7fa
loch <nigelloch@gmail.com>
2022-02-09 23:00:38 -0600

    added models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+});
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: Tag,
+  as: 'tag_product',
+});
 
 module.exports = {
   Product,
